34cbfb77ab95a268740802efa3dbd26db6b0585c
Salador <Salador2005@yandex.ru>
2020-09-16 09:36:18 +0000

    Fix route

--- routes/route.php
+++ routes/route.php
@@ -6,11 +6,7 @@ use Orchids\XSetting\Http\Screens\XSettingList;
 
 Route::screen('xsetting/{xsetting}/edit', XSettingEdit::class)
     ->name('edit');
-
 Route::screen('xsetting/create', XSettingEdit::class)
     ->name('create');
 Route::screen('xsetting', XSettingList::class)
     ->name('list');
-//$this->router->screen('xsetting/{xsetting}/edit', XSettingEdit::class)->name($path.'edit');
-//$this->router->screen('xsetting/create', XSettingEdit::class)->name($path.'create');
-//$this->router->screen('xsetting', XSettingList::class)->name($path.'list');
